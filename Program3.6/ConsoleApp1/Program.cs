7ecc6cc31c05f1e095aa5f22254af7b3ac4795dd
Bryant Wong <bryant52376@gmail.com>
2021-03-07 14:01:05 +0800

    專案初始化

---
+++ Program3.6/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathLibrary;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("123");
+            int result = MathCore.Add(5, 6);
+            Console.WriteLine("5 + 6 =" + result);
+
+            Console.ReadKey();
+#if DEBUG
+            Console.WriteLine(1);
+#endif
+
+
+#if RELEASE
+            Console.WriteLine(1);
+#endif
+        }
+
+        static void GOGOGO()
+        {
+            int result = MathCore.Add(5, 6);
+        }
+
+    }
+}
---
+++ Program3.6/FileConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "D:\\CSharp\\";
+            //string path = "Fake\\";
+            string fileName = "AAA.txt";
+            string dirName = "ccc";
+            //string content = TestFileReader.ReadFile(path + fileName);
+            //Console.WriteLine(content);
+            //string content = "hello world";
+            //TestFileReader.Append(path + fileName, content);
+            //TestFolderReader.ListAllFile(path);
+            TestFolderReader.CreateFolder(path + dirName);
+            Console.Read();
+        }
+    }
+}
